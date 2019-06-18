4d207d70d16369aab2ce9a5196232d4692cdd2ff
Rick Smith <rick.smith@connectchildcare.com>
2019-06-18 16:42:16 +0100

    fixed asyc issue

--- angles.js
+++ angles.js
@@ -3,18 +3,18 @@ const getRadians = require("@schwingbat/relative-angle").radians;
 // const fs = require('fs');
 const fs = require('mz/fs');
 
-const data = () => fs.readFile('./data.json').then(contents => {
+const data = async () => await fs.readFile('./data.json').then(contents => {
     let result = [];
     let arr = JSON.parse(contents);
-    console.log(arr.length, arr)
     for (let i = 0; i < arr.length-1; i++) {
         const pointOne = {x: arr[i].position.x, y: arr[i].position.y};
         const pointTwo = {x: arr[i + 1].position.x, y: arr[i + 1].position.y};
         result.push(getDegrees(pointOne, pointTwo))
     }
-    console.log(result, result.length)
     return result;
 }).catch(console.log)
 
-data()
+data().then((data) => {
+    console.log(data)
+});
 
